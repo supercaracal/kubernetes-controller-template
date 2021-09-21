ad8ee86e3c50de949b85e0ecae186b233e86a870
Taishi Kasuga <kasuga@livesense.co.jp>
2021-09-21 16:38:50 +0900

    Fix syntax error

--- scripts/wait_pod_running.sh
+++ scripts/wait_pod_running.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
+readonly NAME=$1
+
 while :
 do
-  status=$(kubectl --context=kind-kind get pods -o json | jq -r ".items[] | select(.metadata.name | contains('$1')) | .status.phase")
+  status=$(kubectl --context=kind-kind get pods -o json | jq -r ".items[] | select(.metadata.name | contains(\"${NAME}\")) | .status.phase")
   if [[ $status = 'Running' ]]; then
     break
   fi
-  echo "Waiting for $1 to be ready..."
+  echo "Waiting for ${NAME} to be ready..."
   sleep 3
 done
