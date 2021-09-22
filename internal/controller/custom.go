ce1b01119a6455446786cc5ce4dd276c6b2475fc
Taishi Kasuga <kasuga@livesense.co.jp>
2021-09-22 22:58:25 +0900

    Add kubernetes client

--- internal/controller/custom.go
+++ internal/controller/custom.go
@@ -6,7 +6,10 @@ import (
 
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/kubernetes/scheme"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	kubescheme "k8s.io/client-go/kubernetes/scheme"
+	corelisterv1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
@@ -14,10 +17,10 @@ import (
 
 	handlers "github.com/supercaracal/kubernetes-controller-template/internal/handler"
 	workers "github.com/supercaracal/kubernetes-controller-template/internal/worker"
-	clientset "github.com/supercaracal/kubernetes-controller-template/pkg/generated/clientset/versioned"
+	customclient "github.com/supercaracal/kubernetes-controller-template/pkg/generated/clientset/versioned"
 	customscheme "github.com/supercaracal/kubernetes-controller-template/pkg/generated/clientset/versioned/scheme"
-	informers "github.com/supercaracal/kubernetes-controller-template/pkg/generated/informers/externalversions"
-	informerv1 "github.com/supercaracal/kubernetes-controller-template/pkg/generated/informers/externalversions/supercaracal/v1"
+	custominformers "github.com/supercaracal/kubernetes-controller-template/pkg/generated/informers/externalversions"
+	customlisterv1 "github.com/supercaracal/kubernetes-controller-template/pkg/generated/listers/supercaracal/v1"
 )
 
 const (
@@ -28,41 +31,63 @@ const (
 
 // CustomController is
 type CustomController struct {
-	customClientSet       clientset.Interface
-	customInformerFactory informers.SharedInformerFactory
-	customInformer        informerv1.FooBarInformer
-	workQueue             workqueue.RateLimitingInterface
-	reconcileDuration     time.Duration
+	kube              *kubeTool
+	custom            *customTool
+	workQueue         workqueue.RateLimitingInterface
+	reconcileDuration time.Duration
+}
+
+type kubeTool struct {
+	client  kubernetes.Interface
+	factory kubeinformers.SharedInformerFactory
+	pod     *podInfo
+}
+
+type customTool struct {
+	client   customclient.Interface
+	factory  custominformers.SharedInformerFactory
+	resource *customResourceInfo
+}
+
+type podInfo struct {
+	informer cache.SharedIndexInformer
+	lister   corelisterv1.PodLister
+}
+
+type customResourceInfo struct {
+	informer cache.SharedIndexInformer
+	lister   customlisterv1.FooBarLister
 }
 
 // NewCustomController is
 func NewCustomController(cfg *rest.Config) (*CustomController, error) {
-	if err := customscheme.AddToScheme(scheme.Scheme); err != nil {
+	if err := customscheme.AddToScheme(kubescheme.Scheme); err != nil {
 		return nil, err
 	}
 
-	cli, err := clientset.NewForConfig(cfg)
+	kube, err := buildKubeTools(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	infoF := informers.NewSharedInformerFactory(cli, informerReSyncDuration)
-	info := infoF.Supercaracal().V1().FooBars()
+	custom, err := buildCustomResourceTools(cfg)
+	if err != nil {
+		return nil, err
+	}
 
 	wq := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), resourceName)
 	h := handlers.NewInformerHandler(wq)
-	info.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+	custom.resource.informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    h.OnAdd,
 		UpdateFunc: h.OnUpdate,
 		DeleteFunc: h.OnDelete,
 	})
 
 	controller := CustomController{
-		customClientSet:       cli,
-		customInformerFactory: infoF,
-		customInformer:        info,
-		workQueue:             wq,
-		reconcileDuration:     reconcileDuration,
+		kube:              kube,
+		custom:            custom,
+		workQueue:         wq,
+		reconcileDuration: reconcileDuration,
 	}
 
 	return &controller, nil
@@ -73,13 +98,24 @@ func (c *CustomController) Run(stopCh <-chan struct{}) error {
 	defer utilruntime.HandleCrash()
 	defer c.workQueue.ShutDown()
 
-	c.customInformerFactory.Start(stopCh)
+	c.kube.factory.Start(stopCh)
+	c.custom.factory.Start(stopCh)
 
-	if ok := cache.WaitForCacheSync(stopCh, c.customInformer.Informer().HasSynced); !ok {
+	if ok := cache.WaitForCacheSync(stopCh, c.kube.pod.informer.HasSynced, c.custom.resource.informer.HasSynced); !ok {
 		return fmt.Errorf("failed to wait for caches to sync")
 	}
 
-	worker := workers.NewReconciler(c.customClientSet, c.customInformer.Lister(), c.workQueue)
+	worker := workers.NewReconciler(
+		&workers.ResourceClient{
+			Kube:   c.kube.client,
+			Custom: c.custom.client,
+		},
+		&workers.ResourceLister{
+			Pod:            c.kube.pod.lister,
+			CustomResource: c.custom.resource.lister,
+		},
+		c.workQueue,
+	)
 	go wait.Until(worker.Run, c.reconcileDuration, stopCh)
 
 	klog.Info("Controller is ready")
@@ -87,4 +123,36 @@ func (c *CustomController) Run(stopCh <-chan struct{}) error {
 	klog.Info("Shutting down controller")
 
 	return nil
+}
+
+func buildKubeTools(cfg *rest.Config) (*kubeTool, error) {
+	cli, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	info := kubeinformers.NewSharedInformerFactory(cli, informerReSyncDuration)
+	pod := info.Core().V1().Pods()
+
+	return &kubeTool{
+		client:  cli,
+		factory: info,
+		pod:     &podInfo{informer: pod.Informer(), lister: pod.Lister()},
+	}, nil
+}
+
+func buildCustomResourceTools(cfg *rest.Config) (*customTool, error) {
+	cli, err := customclient.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	info := custominformers.NewSharedInformerFactory(cli, informerReSyncDuration)
+	cr := info.Supercaracal().V1().FooBars()
+
+	return &customTool{
+		client:   cli,
+		factory:  info,
+		resource: &customResourceInfo{informer: cr.Informer(), lister: cr.Lister()},
+	}, nil
 }
--- internal/worker/reconciler.go
+++ internal/worker/reconciler.go
@@ -6,30 +6,40 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes"
+	corelisterv1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
-	"k8s.io/cri-api/pkg/errors"
+	kubeerrors "k8s.io/cri-api/pkg/errors"
 	"k8s.io/klog/v2"
 
 	customapiv1 "github.com/supercaracal/kubernetes-controller-template/pkg/apis/supercaracal/v1"
-	clientset "github.com/supercaracal/kubernetes-controller-template/pkg/generated/clientset/versioned"
-	listers "github.com/supercaracal/kubernetes-controller-template/pkg/generated/listers/supercaracal/v1"
+	customclient "github.com/supercaracal/kubernetes-controller-template/pkg/generated/clientset/versioned"
+	customlisterv1 "github.com/supercaracal/kubernetes-controller-template/pkg/generated/listers/supercaracal/v1"
 )
 
 // Reconciler is
 type Reconciler struct {
-	customClientSet clientset.Interface
-	customLister    listers.FooBarLister
-	workQueue       workqueue.RateLimitingInterface
+	client    *ResourceClient
+	lister    *ResourceLister
+	workQueue workqueue.RateLimitingInterface
+}
+
+// ResourceClient is
+type ResourceClient struct {
+	Kube   kubernetes.Interface
+	Custom customclient.Interface
+}
+
+// ResourceLister is
+type ResourceLister struct {
+	Pod            corelisterv1.PodLister
+	CustomResource customlisterv1.FooBarLister
 }
 
 // NewReconciler is
-func NewReconciler(cli clientset.Interface, lst listers.FooBarLister, wq workqueue.RateLimitingInterface) *Reconciler {
-	return &Reconciler{
-		customClientSet: cli,
-		customLister:    lst,
-		workQueue:       wq,
-	}
+func NewReconciler(cli *ResourceClient, list *ResourceLister, wq workqueue.RateLimitingInterface) *Reconciler {
+	return &Reconciler{client: cli, lister: list, workQueue: wq}
 }
 
 // Run is
@@ -78,9 +88,9 @@ func (r *Reconciler) do(key string) error {
 		return fmt.Errorf("invalid resource key: %s: %w", key, err)
 	}
 
-	resource, err := r.customLister.FooBars(namespace).Get(name)
+	resource, err := r.lister.CustomResource.FooBars(namespace).Get(name)
 	if err != nil {
-		if errors.IsNotFound(err) {
+		if kubeerrors.IsNotFound(err) {
 			return fmt.Errorf("custom resource '%s' in work queue no longer exists: %w", key, err)
 		}
 
@@ -94,6 +104,6 @@ func (r *Reconciler) do(key string) error {
 func (r *Reconciler) updateCustomResourceStatus(resource *customapiv1.FooBar) (err error) {
 	cpy := resource.DeepCopy()
 	cpy.Status.Succeeded = true
-	_, err = r.customClientSet.SupercaracalV1().FooBars(resource.Namespace).Update(context.TODO(), cpy, metav1.UpdateOptions{})
+	_, err = r.client.Custom.SupercaracalV1().FooBars(resource.Namespace).Update(context.TODO(), cpy, metav1.UpdateOptions{})
 	return
 }
